4b32ebdd9095045d551028f4e081d1d3f45cf459
fathimanidha <fnidha788@gmail.com>
2026-06-11 10:13:22 +0530

    adding vercel json

--- Server.js
+++ Server.js
@@ -20,7 +20,7 @@ connectDb().then(() => {
 
 const app=express()
 
-const PORT=3000;
+const PORT = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
